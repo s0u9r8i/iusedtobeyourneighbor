2c02f5b912cde969375c6d425549ad8240f420ae
suri <159725087+s0u9r8i@users.noreply.github.com>
2026-06-24 19:12:42 -0400

    Add files via upload

---
+++ pigeon-data.js
@@ -0,0 +1,461 @@
+window.PIGEON_PLACEMENTS = [
+  {
+    "id": 1,
+    "name": "FEATHER",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_01.png?v=1781807379",
+    "col": 13,
+    "row": 16,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/feather"
+  },
+  {
+    "id": 2,
+    "name": "RUSTY",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_02.png?v=1781807379",
+    "col": 44,
+    "row": 7,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/rusty"
+  },
+  {
+    "id": 3,
+    "name": "SUSAN",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_03.png?v=1781981288",
+    "col": 12,
+    "row": 20,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/susan"
+  },
+  {
+    "id": 4,
+    "name": "TONY",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_04.png?v=1781807379",
+    "col": 31,
+    "row": 3,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/tony"
+  },
+  {
+    "id": 5,
+    "name": "CHRISTOPHER",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_05.png?v=1781807379",
+    "col": 40,
+    "row": 4,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/christopher"
+  },
+  {
+    "id": 6,
+    "name": "JESSICA",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_06.png?v=1781807380",
+    "col": 56,
+    "row": 7,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/jessica"
+  },
+  {
+    "id": 7,
+    "name": "SKITTER",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_07.png?v=1781807379",
+    "col": 33,
+    "row": 16,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/skitter"
+  },
+  {
+    "id": 8,
+    "name": "JACKSON",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_08.png?v=1781807379",
+    "col": 13,
+    "row": 20,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/jackson"
+  },
+  {
+    "id": 9,
+    "name": "BARBARA",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_09.png?v=1781807379",
+    "col": 27,
+    "row": 17,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/barbara"
+  },
+  {
+    "id": 10,
+    "name": "PEEPER",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_10.png?v=1781807379",
+    "col": 12,
+    "row": 20,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/peeper"
+  },
+  {
+    "id": 11,
+    "name": "DAVID",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_11.png?v=1781981210",
+    "col": 36,
+    "row": 18,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/david"
+  },
+  {
+    "id": 12,
+    "name": "FRANKIE",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_12.png?v=1781981210",
+    "col": 12,
+    "row": 23,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/frankie"
+  },
+  {
+    "id": 13,
+    "name": "DONNA",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_13.png?v=1781981210",
+    "col": 20,
+    "row": 3,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/donna"
+  },
+  {
+    "id": 14,
+    "name": "PATTY",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_14.png?v=1781981210",
+    "col": 30,
+    "row": 17,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/patty"
+  },
+  {
+    "id": 15,
+    "name": "NUZZLE",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_15.png?v=1781981210",
+    "col": 11,
+    "row": 16,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/nuzzle"
+  },
+  {
+    "id": 16,
+    "name": "COO",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_16.png?v=1781981209",
+    "col": 8,
+    "row": 20,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/coo"
+  },
+  {
+    "id": 17,
+    "name": "MARCO",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_17.png?v=1781981212",
+    "col": 15,
+    "row": 16,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/marco"
+  },
+  {
+    "id": 18,
+    "name": "SHELLY",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_18.png?v=1781981211",
+    "col": 12,
+    "row": 25,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/shelly"
+  },
+  {
+    "id": 19,
+    "name": "PAUL",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_19.png?v=1781981211",
+    "col": 33,
+    "row": 17,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/paul"
+  },
+  {
+    "id": 20,
+    "name": "HOPPER",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_20.png?v=1781981211",
+    "col": 27,
+    "row": 17,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/hopper"
+  },
+  {
+    "id": 21,
+    "name": "FRANCESCA",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_21.png?v=1781981212",
+    "col": 12,
+    "row": 16,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/francesca"
+  },
+  {
+    "id": 22,
+    "name": "TWEETY",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_22.png?v=1781981213",
+    "col": 43,
+    "row": 5,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/tweety"
+  },
+  {
+    "id": 23,
+    "name": "ALEX",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_23.png?v=1781981213",
+    "col": 11,
+    "row": 21,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/alex"
+  },
+  {
+    "id": 24,
+    "name": "EMMA",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_24.png?v=1781981212",
+    "col": 14,
+    "row": 16,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/emma"
+  },
+  {
+    "id": 25,
+    "name": "PETER",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_25.png?v=1781981212",
+    "col": 12,
+    "row": 28,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/peter"
+  },
+  {
+    "id": 26,
+    "name": "ROMEO",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_26.png?v=1781981213",
+    "col": 45,
+    "row": 6,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/romeo"
+  },
+  {
+    "id": 27,
+    "name": "LOFTY",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_27.png?v=1781981212",
+    "col": 56,
+    "row": 9,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/lofty"
+  },
+  {
+    "id": 28,
+    "name": "DANIEL",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_28.png?v=1781981212",
+    "col": 27,
+    "row": 4,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/daniel"
+  },
+  {
+    "id": 29,
+    "name": "ANGELA",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_29.png?v=1781981213",
+    "col": 21,
+    "row": 3,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/angela"
+  },
+  {
+    "id": 30,
+    "name": "SOPHIA",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_30.png?v=1781981213",
+    "col": 12,
+    "row": 25,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/sophia"
+  },
+  {
+    "id": 31,
+    "name": "KAREN",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_31.png?v=1781981212",
+    "col": 43,
+    "row": 7,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/karen"
+  },
+  {
+    "id": 32,
+    "name": "PEACE",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_32.png?v=1781981211",
+    "col": 12,
+    "row": 21,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/peace"
+  },
+  {
+    "id": 33,
+    "name": "BEAKY",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_33.png?v=1781981210",
+    "col": 11,
+    "row": 19,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/beaky"
+  },
+  {
+    "id": 34,
+    "name": "MICHAEL",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_34.png?v=1781981209",
+    "col": 28,
+    "row": 17,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/michael"
+  },
+  {
+    "id": 35,
+    "name": "JULIET",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_35.png?v=1781981212",
+    "col": 43,
+    "row": 6,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/juliet"
+  },
+  {
+    "id": 36,
+    "name": "CHIP",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_36.png?v=1781981211",
+    "col": 54,
+    "row": 7,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/chip"
+  },
+  {
+    "id": 37,
+    "name": "STEVEN",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_37.png?v=1781981211",
+    "col": 51,
+    "row": 12,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/steven"
+  },
+  {
+    "id": 39,
+    "name": "NESTLE",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_39.png?v=1781981212",
+    "col": 12,
+    "row": 27,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/nestle"
+  },
+  {
+    "id": 40,
+    "name": "MARIA",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_40.png?v=1781981211",
+    "col": 42,
+    "row": 4,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/maria"
+  },
+  {
+    "id": 41,
+    "name": "NICK",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_41.png?v=1781981212",
+    "col": 26,
+    "row": 0,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/nick"
+  },
+  {
+    "id": 42,
+    "name": "JENNIFER",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_42.png?v=1781981212",
+    "col": 32,
+    "row": 7,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/jennifer"
+  },
+  {
+    "id": 43,
+    "name": "SWEETIE",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_43.png?v=1781981212",
+    "col": 54,
+    "row": 10,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/sweetie"
+  },
+  {
+    "id": 44,
+    "name": "TERESA",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_44.png?v=1781981213",
+    "col": 54,
+    "row": 8,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/teresa"
+  },
+  {
+    "id": 45,
+    "name": "SAM",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_45.png?v=1781981210",
+    "col": 39,
+    "row": 6,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/sam"
+  },
+  {
+    "id": 46,
+    "name": "CHRISTINA",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_46.png?v=1781981208",
+    "col": 25,
+    "row": 4,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/christina"
+  },
+  {
+    "id": 47,
+    "name": "CUPID",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_47.png?v=1781981209",
+    "col": 17,
+    "row": 16,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/cupid"
+  },
+  {
+    "id": 48,
+    "name": "ANNA",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_48.png?v=1781981211",
+    "col": 24,
+    "row": 17,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/anna"
+  },
+  {
+    "id": 49,
+    "name": "BREEZY",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_49.png?v=1781981213",
+    "col": 41,
+    "row": 20,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/breezy"
+  },
+  {
+    "id": 50,
+    "name": "JAMIE",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_50.png?v=1781981213",
+    "col": 43,
+    "row": 2,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/jamie"
+  },
+  {
+    "id": 51,
+    "name": "MARY",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_51.png?v=1781981213",
+    "col": 22,
+    "row": 4,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/mary"
+  },
+  {
+    "id": 52,
+    "name": "JOEY",
+    "inventory": 1,
+    "image": "https://cdn.shopify.com/s/files/1/2263/4647/files/PB_website_52.png?v=1781981213",
+    "col": 44,
+    "row": 1,
+    "productLink": "https://3ssstudios.com/collections/bird-pit-artwork/products/joey"
+  }
+];
